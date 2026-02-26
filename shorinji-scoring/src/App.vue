32f6b0de0cc0428acfeee082a969339ce1af8a45
Stepha-360 <stepha02.sbv@gmail.com>
2026-02-26 12:55:51 -0500

    Refactor App.vue layout and styles
    Refactor App.vue to improve structure and styling.

--- shorinji-scoring/src/App.vue
+++ shorinji-scoring/src/App.vue
@@ -1,38 +1,22 @@
 <script setup>
-    import Panel from './components/panel.vue';
-    import fondo from './assets/shorinji-fondo.png'
+import Panel from './components/panel.vue';
 
 </script>
 
 <template>
-     <div class="contenedor">
-    <Panel variante="red" />
-    <Panel variante="blue" />
-  </div>
 
-       
+  <Panel variante="red" />
+  <Panel variante="blue" />
+   
 </template>
 
 
 <style scoped>
-    div {
-        display: flex;
-        width: 100%;
-        padding: 20px;
-        gap: 20%;
-    }
-
-    .contenedor {
+.contenedor {
   display: flex;
-  width: 50%;
-  height: 60vh; /* ocupa toda la pantalla */
+  width: 100%;
   padding: 20px;
-  gap: 20%;
-
-  background-image: url('./assets/shorinji-fondo.png');
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+  gap: 20px;
 }
 
 </style>
